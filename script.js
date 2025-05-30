5b90ffacc600ce03589e18e332a6d66ac29319e0
thiagohvs12 <thiagohenriquevs@gmail.com>
2025-05-30 20:33:32 -0300

    realização da parte do JS onde criamos o código onde o valor se torna convertido e ai mostra que o resultado normal

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('currency-form').addEventListener('submit',function(event){
+    event.preventDefault();
+
+    //obter valores de entrada
+    const valor   = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+
+    //Definir taxas de cambio através de onjeto dentro do JS
+    const exchangeRates = {
+        USD: { BRL:5.73, EUR:0.88 },
+        BRL: { USD:0.17, EUR:0.15 },
+        EUR: { USD:1.13, BRL:6.50 }
+    };
+
+    let valorConvertido
+    if(daMoeda === paraMoeda){
+        valorConvertido =parseFloat(valor);
+    }else{
+        valorConvertido = parseFloat(valor * exchangeRates[daMoeda][paraMoeda]);
+    }
+
+    const conversao = document.getElementById('conversao');
+    conversao.textContent =`Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda}`;
+    
+});
